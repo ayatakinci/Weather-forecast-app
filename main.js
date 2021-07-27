5ff85f33cec22743ee349d20d782ba4526d84fac
Ayat AL-ZAIDI <76471156+ayat0110@users.noreply.github.com>
2021-07-27 11:10:18 +0300

    Weather app using HTML, CSS & JavaScript

---
+++ main.js
@@ -0,0 +1,26 @@
+const api = {
+    key: "afaf9f8d48cff6cafd32e23220bcfdbf",
+    base: "https://api.openweathermap.org/data/2.5/"
+  }
+  
+  const searchbox = document.querySelector('.search-box');
+  searchbox.addEventListener('keypress', setQuery);
+  
+  function setQuery(evt) {
+    if (evt.keyCode == 13) {
+      getResults(searchbox.value);
+    }
+  }
+  
+  function getResults (query) {
+    fetch(`${api.base}weather?q=${query}&units=metric&APPID=${api.key}`)
+      .then(weather => {
+        return weather.json();
+      }).then(displayResults);
+  }
+  
+  function displayResults (weather) {
+    let city = document.querySelector('.Location .city');
+    city.innerText = `${weather.name}, ${weather.sys.country}`;
+  
+  }
