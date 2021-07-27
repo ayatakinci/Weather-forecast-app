2c77604f909730f9cb0bd4a69bcf3eaccfb1f7ec
Ayat AL-ZAIDI <76471156+ayat0110@users.noreply.github.com>
2021-07-27 13:04:51 +0300

    complete js file

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 const api = {
-    key: "afaf9f8d48cff6cafd32e23220bcfdbf",
+    key: "557c6d65d6ea371afd112f45314db143",
     base: "https://api.openweathermap.org/data/2.5/"
   }
   
@@ -20,7 +20,32 @@ const api = {
   }
   
   function displayResults (weather) {
+    //console.log(weather);
     let city = document.querySelector('.Location .city');
     city.innerText = `${weather.name}, ${weather.sys.country}`;
   
+    let now = new Date();
+    let date = document.querySelector('.Location .date');
+    date.innerText = dateBuilder(now);
+
+    let temp = document.querySelector('.current .temp');
+    temp.innerHTML = `${Math.round(weather.main.temp)}<span>°c</span>`;
+  
+    let weather_el = document.querySelector('.current .weather');
+    weather_el.innerText = weather.weather[0].main;
+  
+    let hilow = document.querySelector('.hi-low');
+    hilow.innerText = `${Math.round(weather.main.temp_min)}°c / ${Math.round(weather.main.temp_max)}°c`;
+  }
+
+  function dateBuilder(d){
+      let  months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+      let days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+
+      let day = days[d.getDay()];
+      let date = d.getDate();
+      let month = months[d.getMonth()];
+      let year = d.getFullYear();
+
+       return `${day} ${date} ${month} ${year}`;
   }
